8cdc3ac992c924a2388433e65c9da93402239af6
Nikshay joshi <nikshayjoshi1@gmail.com>
2026-07-11 00:16:58 +0530

    Time: 8 ms (26.43%), Space: 124.5 MB (54.64%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -8,23 +8,56 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     int pairSum(ListNode* head) {
+//         vector<int> arr;
+//         ListNode* temp = head;
+//         while(temp!=nullptr){
+//             arr.push_back(temp->val);
+//             temp = temp->next;
+//         }
+//         int start = 0;
+//         int end = arr.size()-1;
+//         int twinSum = INT_MIN;
+//         while(start<end){
+//             int sum = arr[start] + arr[end];
+//             twinSum = max(sum,twinSum);
+//             start++;
+//             end--;
+//         }
+//         return twinSum;
+//     }
+// };
+
 class Solution {
+    ListNode* ReverseList(ListNode* head){
+        ListNode* prev = nullptr;
+        ListNode* cur = head;
+        while(cur!=nullptr){
+            ListNode* nextnode = cur->next;
+            cur->next = prev;
+            prev = cur;
+            cur = nextnode;
+        }
+        return prev;
+    }
 public:
     int pairSum(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp!=nullptr){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(fast!=nullptr && fast->next!=nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        int start = 0;
-        int end = arr.size()-1;
+        ListNode* head2 = ReverseList(slow->next);
+        ListNode* p = head;
         int twinSum = INT_MIN;
-        while(start<end){
-            int sum = arr[start] + arr[end];
+        while(head2!=nullptr){
+            int sum = p->val + head2->val;
             twinSum = max(sum,twinSum);
-            start++;
-            end--;
+            p = p->next;
+            head2 = head2->next;
         }
         return twinSum;
     }
