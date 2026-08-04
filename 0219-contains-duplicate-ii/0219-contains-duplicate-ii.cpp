371e6eb2d5d4d5dd4af105f01a87b4ceb8ca17b3
Nikshay joshi <nikshayjoshi1@gmail.com>
2026-08-04 22:56:36 +0530

    Time: 115 ms (16.59%), Space: 116.9 MB (30.59%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -3,25 +3,55 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> set;
         int i=0;
-        bool isDup = false;
         while(i<min(k,int(nums.size()))){
             if(set.count(nums[i])){
-                isDup = true;
-                break;
+                return true;
             }
             set.insert(nums[i]);
             i++;
         }
-        if(isDup) return true;
         while(i<nums.size()){
             if(set.count(nums[i])){
-                isDup = true;
-                break;
+                return true;
             }
             set.insert(nums[i]);
-            set.erase(nums[i-k]);   
+            set.erase(nums[i-k]);
             i++;
         }
-        return isDup;
+        return false;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+ // unordered_set<int> set;
+        // int i=0;
+        // bool isDup = false;
+        // while(i<min(k,int(nums.size()))){
+        //     if(set.count(nums[i])){
+        //         isDup = true;
+        //         break;
+        //     }
+        //     set.insert(nums[i]);
+        //     i++;
+        // }
+        // if(isDup) return true;
+        // while(i<nums.size()){
+        //     if(set.count(nums[i])){
+        //         isDup = true;
+        //         break;
+        //     }
+        //     set.insert(nums[i]);
+        //     set.erase(nums[i-k]);   
+        //     i++;
+        // }
+        // return isDup;
