807df591abee2d56bc7572767b184745cb460c91
Nikshay joshi <nikshayjoshi1@gmail.com>
2026-08-04 23:48:45 +0530

    Time: 308 ms (6.31%), Space: 81.3 MB (7.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // sliding window
         int maxLen = 0;
+        int i=0;
+        int j=0;
         unordered_set<char> set;
-        int i = 0;
-        int j = 0;
         while(i<s.length()){
-            while(set.count(s[i])){  // is duplicate exist in the set
-                set.erase(s[j]);  // erase the first occurance of that ch
-                j+=1;   // reduce the size of the window
+            while(set.count(s[i])){
+                set.erase(s[j]);
+                j+=1;
             }
             set.insert(s[i]);
-            maxLen = max(maxLen,i-j+1);  // update the maxLen with maximum lenghtt of nrc in string
+            maxLen = max(maxLen,i-j+1);
             i+=1;
         }
         return maxLen;
     }
 };
 
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         int maxLen = 0;
-//         int n = s.length();
-//         unordered_set<char> set;
-//         for(int i=0 ; i<n ; i++){
-//             int len = 0;
-//             for(int j = i ; j<n ; j++){
-//                 if(set.count(s[j])){
-//                     // erase all elements of set
-//                     set.clear();
-//                     break;
-//                 }
-//                 set.insert(s[j]);
-//                 len+=1;
-//             }
-//             maxLen = max(len,maxLen);
-//         }
-//         return maxLen;
-//     }
-// };
