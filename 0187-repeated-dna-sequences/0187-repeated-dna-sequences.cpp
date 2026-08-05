bdc9e27560861e67ff2cee81192cb53ef92417e2
Nikshay joshi <nikshayjoshi1@gmail.com>
2026-08-05 22:47:04 +0530

    Time: 61 ms (14.89%), Space: 22.5 MB (71.7%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<string> findRepeatedDnaSequences(string s) {
+        unordered_set<string> seen; // to store all the strings
+        unordered_set<string> set; // to store all the answers
+        vector<string> ans;
+        string str;
+        for(int i=0 ; i<s.length() ; i++){
+            str = "";
+            if(i+10<=s.length()){
+                for(int j=i ; j<i+10 ; j++){
+                    str.push_back(s[j]);
+                }
+            }
+            if(seen.count(str)){
+                set.insert(str);
+            }
+            if(str!="")
+                seen.insert(str);
+        }
+        for(auto str : set){
+            ans.push_back(str);
+        }
+        return ans;
+    }
+};
