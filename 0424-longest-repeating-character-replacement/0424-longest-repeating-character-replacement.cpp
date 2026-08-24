947fd67cbdb74cbd73ae31e7f7da87bbe524aac4
Nikshay joshi <nikshayjoshi1@gmail.com>
2026-08-24 22:38:41 +0530

    Time: 1 ms (75.21%), Space: 10.7 MB (93.67%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,3 +1,35 @@
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+        
+//         int maxFreq = 0;  
+        
+//         for(int i=0 ; i<s.length() ; i++){
+
+//             int maxCh = 0;  frequency of the maximum occuring ch in substr
+//             int chFreq[26] = {0}; array to maintain freq of each uppercase ch inside str
+
+//             for(int j=i ; j<s.length() ; j++){
+
+//                 chFreq[s[j] - 'A']++;   increment freq of current ch and store in array
+
+//                 maxCh = max(maxCh , chFreq[s[j] - 'A']); update maxch with max value inside the substr
+
+//                 int ops = (j-i+1) - maxCh;  no of oprn required ti make elements similar inside substr
+//                 if(ops>k){
+//                     break;
+//                 }
+                
+//                 maxFreq = max(maxFreq,j-i+1); update answer
+//             }
+//         }
+
+//         return maxFreq;
+//     }
+// };
+
+
+//  NO here is the optimized dynamic sliding window solution
 class Solution {
 public:
     int characterReplacement(string s, int k) {
