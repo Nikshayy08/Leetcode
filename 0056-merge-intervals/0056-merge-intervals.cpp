d473b9df590748f41f5830dab251ed2a976de97c
Nikshay joshi <nikshayjoshi1@gmail.com>
2026-09-02 00:09:42 +0530

    Time: 3 ms (89.02%), Space: 24 MB (32.28%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,27 +1,49 @@
+// class Solution {         BRUTE FORCE
+// public:
+//     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//         bool isMerged = true;
+//         while(isMerged){
+//             isMerged = false;
+
+//             for(int i=0 ; i<intervals.size() ; i++){
+//                 int j = i+1;
+//                 while(j<intervals.size()){
+//                     if(intervals[i][1]>=intervals[j][0] && intervals[j][1]>=intervals[i][0]){
+//                         intervals[i][0] = min(intervals[i][0],intervals[j][0]);
+//                         intervals[i][1] = max(intervals[i][1],intervals[j][1]);
+//                         intervals.erase(intervals.begin() + j); 
+//                         // intervals.begin() → gives you an iterator pointing to the first element of the vector.
+//                         // + j → moves that iterator forward by j positions.
+//                         isMerged = true;
+//                     }
+//                     else{
+//                         j+=1;
+//                     }
+//                 }
+//             }
+//         }
+//         return intervals;
+//     }
+// };
+
+
+
+//       OPTIMIZED APPROACH
+
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        bool isMerged = true;
-        while(isMerged){
-            isMerged = false;
-
-            for(int i=0 ; i<intervals.size() ; i++){
-                int j = i+1;
-                while(j<intervals.size()){
-                    if(intervals[i][1]>=intervals[j][0] && intervals[j][1]>=intervals[i][0]){
-                        intervals[i][0] = min(intervals[i][0],intervals[j][0]);
-                        intervals[i][1] = max(intervals[i][1],intervals[j][1]);
-                        intervals.erase(intervals.begin() + j); 
-                        // intervals.begin() → gives you an iterator pointing to the first element of the vector.
-                        // + j → moves that iterator forward by j positions.
-                        isMerged = true;
-                    }
-                    else{
-                        j+=1;
-                    }
-                }
+        sort(intervals.begin(),intervals.end());
+        vector<vector<int>> merged;
+        for(int i=0 ; i<intervals.size() ; i++){
+            // check overlap 
+            if(merged.empty() || merged.back()[1]<intervals[i][0]){  // if its the first interval || if intervals doesnt overlap
+                merged.push_back(intervals[i]);
+            }
+            else{ // overlap exist
+                merged.back()[1] = max(merged.back()[1],intervals[i][1]);
             }
         }
-        return intervals;
+        return merged;
     }
 };
